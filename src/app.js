304a5456ba2b3cff734ccf659d18320b18698734
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-12 10:31:10 -0300

    added connection test

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from 'express';
 import getMessage from './getMessage';
+import ADODB from 'node-adodb';
 
 var app = express();
 
 var port = 4000;
+
+
+// Connect to the MS Access DB
+const connection = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\DATABASE\\steam_games.accdb;Persist Security Info=False;');
+
+
+
+
+async function query() {
+  try {
+    const users = await connection.query('SELECT * FROM [Origin Games]');
+ 
+    console.log(JSON.stringify(users, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
+}
+ 
+query();
+ 
 
 app.get('/', async (req, res) => {
   await new Promise((resolve, reject) => {
