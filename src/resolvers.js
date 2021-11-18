9bad7ef4a9e1db4a54dbd6f044446139f6bdb753
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-18 08:02:55 -0300

    create crud for wiiu_games in graphql

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,10 +1,34 @@
 // The root provides a resolver function for each API endpoint
-export const root = {
-    hello: () => {
-      return 'Hello world!';
-    },
+export const root = {    
     allWiiUGames: async (_,ctx) => {        
-        const games = ctx.db.query('SELECT * FROM [wiiu_games]')
+        const games = await ctx.db.query('SELECT * FROM [wiiu_games]')
         return games;
+    },
+    getWiiUGame: async ({id}, ctx) => {      
+        const game = await ctx.db.query(`SELECT * FROM [wiiu_games] WHERE [id] = '${id}'`)
+        return game[0];        
+    },
+    createWiiUGame: async (_, ctx) => {
+      const {id,title,finished,fisical_disc} = _.input;
+      await ctx.db.execute(`INSERT INTO [wiiu_games] (id,title,finished,fisical_disc) VALUES ('${id}','${title}',${finished},${fisical_disc});`)
+      const game = await ctx.db.query(`SELECT * FROM [wiiu_games] WHERE [id] = '${id}'AND [title] = '${title}'`)      
+      return game[0];        
+    },
+    updateWiiUGame: async (_, ctx) => {
+      const {id,idx,title,finished,fisical_disc} = _.input;
+      await ctx.db.execute(`UPDATE [wiiu_games] SET [id] = '${id}',[title] = '${title}', [finished] = ${finished}, [fisical_disc] = ${fisical_disc} WHERE [idx] = ${idx};`)
+      const game = await ctx.db.query(`SELECT * FROM [wiiu_games] WHERE [idx] = ${idx}`)      
+      return game[0];        
+    },
+    deleteWiiUGame: async ({idx}, ctx) => {
+      let resp;
+      try {
+        await ctx.db.execute(`DELETE FROM [wiiu_games] WHERE [idx] = ${idx};`)  
+        resp = true;
+      } catch (error) {
+        console.error(error)
+        resp = false
+      }      
+      return resp;
     }
   };  
--- src/schema.js
+++ src/schema.js
@@ -1,17 +1,33 @@
 import { buildSchema } from 'graphql';
 
+const inputsWiiU = `
+  id: String
+  title: String
+  finished: Boolean
+  fisical_disc: Boolean  
+` 
+
 // Construct a schema, using GraphQL schema language
 export const schema = buildSchema(`
+  input WiiUGameInput {
+    ${inputsWiiU}
+  }
+  input WiiUGameUpdateInput {
+    idx: ID
+    ${inputsWiiU}  
+  }
   type WiiUGame {
     idx: ID
-    id: String
-    title: String
-    finished: Boolean
-    fisical_disc: Boolean  
+    ${inputsWiiU}  
   }  
-  type Query {
-    hello: String
+  type Query {    
     allWiiUGames: [WiiUGame!]!
+    getWiiUGame(id: String!): WiiUGame
+  }
+  type Mutation {
+    createWiiUGame(input: WiiUGameInput): WiiUGame
+    updateWiiUGame(input: WiiUGameUpdateInput): WiiUGame
+    deleteWiiUGame(idx: ID!): Boolean
   }
 `);
 
