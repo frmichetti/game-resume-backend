706b69cf6d1d72bec2d8519b9bd1735c214fa91f
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-18 09:36:37 -0300

    create crud for origin and ubisoft games in graphql

--- src/schema.js
+++ src/schema.js
@@ -17,6 +17,17 @@ const inputWiiGCGame= `
   size_gb: String
 `;
 
+const inputOriginGame = `
+  id: String
+  title: String
+  finished: Boolean
+`
+const inputUbisoftGame = `
+  id: String
+  title: String
+  finished: Boolean
+`
+
 // Construct a schema, using GraphQL schema language
 export const schema = buildSchema(`
   input WiiUGameInput {
@@ -25,13 +36,27 @@ export const schema = buildSchema(`
   input WiiGCGameInput {
     ${inputWiiGCGame}
   }
+  input OriginGameInput{
+    ${inputOriginGame}
+  }
+  input UbisoftGameInput{
+    ${inputUbisoftGame}
+  }
   input WiiGCGameUpdateInput {
     idx: ID
     ${inputWiiGCGame}
-  }
+  }  
   input WiiUGameUpdateInput {
     idx: ID
     ${inputsWiiU}  
+  }
+  input OriginUpdateGameInput {
+    idx: ID
+    ${inputOriginGame}  
+  }
+  input UbisoftUpdateGameInput {
+    idx: ID
+    ${inputUbisoftGame}  
   }
   type WiiUGame {
     idx: ID
@@ -41,19 +66,37 @@ export const schema = buildSchema(`
     idx: ID
     ${inputWiiGCGame}
   }  
+  type OriginGame {
+    idx: ID
+    ${inputOriginGame}
+  }
+  type UbisoftGame {
+    idx: ID
+    ${inputUbisoftGame}
+  }
   type Query {    
     allWiiUGames: [WiiUGame!]!
     allWiiGCGames: [WiiGCGame!]!
+    allOriginGames: [OriginGame!]!
+    allUbisoftGames: [UbisoftGame!]!
     getWiiUGame(id: String!): WiiUGame
     getWiiGCGame(id: String!): WiiGCGame
+    getOriginGame(idx: ID!): OriginGame
+    getUbisoftGame(idx: ID!): UbisoftGame
   }
   type Mutation {
     createWiiUGame(input: WiiUGameInput): WiiUGame
     createWiiGCGame(input: WiiGCGameInput): WiiGCGame
+    createOriginGame(input: OriginGameInput): OriginGame
+    createUbisoftGame(input: UbisoftGameInput): UbisoftGame
     updateWiiUGame(input: WiiUGameUpdateInput): WiiUGame
     updateWiiGCGame(input: WiiGCGameUpdateInput): WiiGCGame
+    updateOriginGame(input: OriginUpdateGameInput): OriginGame
+    updateUbisoftGame(input: UbisoftUpdateGameInput): UbisoftGame
     deleteWiiUGame(idx: ID!): Boolean
     deleteWiiGCGame(idx: ID!): Boolean
+    deleteOriginGame(idx: ID!): Boolean
+    deleteUbisoftGame(idx: ID!): Boolean
   }
 `);
 
