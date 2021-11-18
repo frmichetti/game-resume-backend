b3a893cca51cedb3c4be41e1cf63c2f58d8e6b86
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-18 08:40:03 -0300

    create crud for wii_gc_games in graphql

--- src/resolvers.js
+++ src/resolvers.js
@@ -4,14 +4,28 @@ export const root = {
         const games = await ctx.db.query('SELECT * FROM [wiiu_games]')
         return games;
     },
+    allWiiGCGames: async (_,ctx) => {        
+      const games = await ctx.db.query('SELECT * FROM [wii_gc_games]')
+      return games;
+  },
     getWiiUGame: async ({id}, ctx) => {      
         const game = await ctx.db.query(`SELECT * FROM [wiiu_games] WHERE [id] = '${id}'`)
         return game[0];        
     },
+    getWiiGCGame: async ({id}, ctx) => {      
+      const game = await ctx.db.query(`SELECT * FROM [wii_gc_games] WHERE [id] = '${id}'`)
+      return game[0];        
+  },
     createWiiUGame: async (_, ctx) => {
       const {id,title,finished,fisical_disc} = _.input;
       await ctx.db.execute(`INSERT INTO [wiiu_games] (id,title,finished,fisical_disc) VALUES ('${id}','${title}',${finished},${fisical_disc});`)
       const game = await ctx.db.query(`SELECT * FROM [wiiu_games] WHERE [id] = '${id}'AND [title] = '${title}'`)      
+      return game[0];        
+    },
+    createWiiGCGame: async (_, ctx) => {
+      const {id,title,finished,fisical_disc, size_gb, iso_type, console} = _.input;
+      await ctx.db.execute(`INSERT INTO [wii_gc_games] (id,title,finished,fisical_disc,size_gb,iso_type,console) VALUES ('${id}','${title}',${finished},${fisical_disc},'${size_gb}', '${iso_type}', '${console}');`)
+      const game = await ctx.db.query(`SELECT * FROM [wii_gc_games] WHERE [id] = '${id}'AND [title] = '${title}'`)      
       return game[0];        
     },
     updateWiiUGame: async (_, ctx) => {
@@ -20,10 +34,27 @@ export const root = {
       const game = await ctx.db.query(`SELECT * FROM [wiiu_games] WHERE [idx] = ${idx}`)      
       return game[0];        
     },
+    updateWiiGCGame: async (_, ctx) => {
+      const {id,idx,title,finished,fisical_disc, size_gb, iso_type, console} = _.input;
+      await ctx.db.execute(`UPDATE [wii_gc_games] SET [id] = '${id}',[title] = '${title}', [finished] = ${finished}, [fisical_disc] = ${fisical_disc}, [size_gb] = '${size_gb}', [iso_type] = '${iso_type}', [console] = '${console}' WHERE [idx] = ${idx};`)
+      const game = await ctx.db.query(`SELECT * FROM [wii_gc_games] WHERE [idx] = ${idx}`)      
+      return game[0];        
+    },
     deleteWiiUGame: async ({idx}, ctx) => {
       let resp;
       try {
         await ctx.db.execute(`DELETE FROM [wiiu_games] WHERE [idx] = ${idx};`)  
+        resp = true;
+      } catch (error) {
+        console.error(error)
+        resp = false
+      }      
+      return resp;
+    },
+    deleteWiiGCGame: async ({idx}, ctx) => {
+      let resp;
+      try {
+        await ctx.db.execute(`DELETE FROM [wii_gc_games] WHERE [idx] = ${idx};`)  
         resp = true;
       } catch (error) {
         console.error(error)
--- src/schema.js
+++ src/schema.js
@@ -5,12 +5,29 @@ const inputsWiiU = `
   title: String
   finished: Boolean
   fisical_disc: Boolean  
-` 
+`;
+
+const inputWiiGCGame= `
+  id: String
+  title: String
+  iso_type: String
+  console: String
+  finished: Boolean
+  fisical_disc: Boolean
+  size_gb: String
+`;
 
 // Construct a schema, using GraphQL schema language
 export const schema = buildSchema(`
   input WiiUGameInput {
     ${inputsWiiU}
+  }
+  input WiiGCGameInput {
+    ${inputWiiGCGame}
+  }
+  input WiiGCGameUpdateInput {
+    idx: ID
+    ${inputWiiGCGame}
   }
   input WiiUGameUpdateInput {
     idx: ID
@@ -19,15 +36,24 @@ export const schema = buildSchema(`
   type WiiUGame {
     idx: ID
     ${inputsWiiU}  
+  }
+  type WiiGCGame {
+    idx: ID
+    ${inputWiiGCGame}
   }  
   type Query {    
     allWiiUGames: [WiiUGame!]!
+    allWiiGCGames: [WiiGCGame!]!
     getWiiUGame(id: String!): WiiUGame
+    getWiiGCGame(id: String!): WiiGCGame
   }
   type Mutation {
     createWiiUGame(input: WiiUGameInput): WiiUGame
+    createWiiGCGame(input: WiiGCGameInput): WiiGCGame
     updateWiiUGame(input: WiiUGameUpdateInput): WiiUGame
+    updateWiiGCGame(input: WiiGCGameUpdateInput): WiiGCGame
     deleteWiiUGame(idx: ID!): Boolean
+    deleteWiiGCGame(idx: ID!): Boolean
   }
 `);
 
