c9c104d42af584a3d636d245f059bc94f1b1a5fd
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-19 13:24:04 -0300

    prevent from crash

--- src/dataloader.js
+++ src/dataloader.js
@@ -1,5 +1,5 @@
 import DataLoader from "dataloader"
-
+import _ from "lodash";
 class DLCLoader {
     /**
      * TODO: FIXME only works on getWiiUGame(id: "")
@@ -9,8 +9,21 @@ class DLCLoader {
         let sql = `SELECT * FROM [dlcs] WHERE id IN (${idsString}) ORDER BY id ASC;`;
         console.log("data loader sql: ", sql)
         const result = await connection.query(sql);
+
+        const arr = []        
+
+        if(ids.length == 1 ){
+            arr.push(result)
+        } else{
+            ids.forEach(id => {
+                arr.push([])
+            });
+            // TODO FIX ME order array
+            arr.pop()
+            arr.push(result)
+        }
         
-        return Promise.resolve([result]);
+        return Promise.resolve(arr);
     }
 }
 
