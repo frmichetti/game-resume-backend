7c1ac41b2e27d7f0458e7086b3f522696397fbe8
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-19 14:15:28 -0300

    fixes

--- src/dataloader.js
+++ src/dataloader.js
@@ -1,40 +1,41 @@
 import DataLoader from "dataloader"
 import _ from "lodash";
-class DLCLoader {
-    /**
-     * TODO: FIXME only works on getWiiUGame(id: "")
-     */    
+class DLCLoader {    
     static async batchDlcs(connection, ids) {
         let idsString = ids.map(v => `"${v}"`).toString();
         let sql = `SELECT * FROM [dlcs] WHERE id IN (${idsString}) ORDER BY id ASC;`;
         console.log("data loader sql: ", sql)
-        const result = await connection.query(sql);
+        const dlcs = await connection.query(sql);
 
-        const arr = []        
+        const arr = []
 
-        if(ids.length == 1 ){
-            arr.push(result)
-        } else{
-            ids.forEach(id => {
-                arr.push([])
-            });
-            // TODO FIX ME order array
-            arr.pop()
-            arr.push(result)
+        // cria um objeto js onde cada chave é um id de dlc
+        // e o valor, o dlc em si
+        const dlcsMap = dlcs.reduce((prev, dlc) => {            
+            return { ...prev, [dlc.id]: [prev, dlc] }
         }
+        );
         
-        return Promise.resolve(arr);
+        let ordened;
+
+        // garante que os dlcs serão retornados na mesma ordem
+        // do array de ids
+        ordened = ids.map(id => dlcsMap[id])
+        
+        ordened = ordened.map(o => o == undefined ? [] : o)
+
+        return Promise.resolve(ordened);
     }
 }
 
-export class DataLoaderFactory {    
-    constructor(connection){
+export class DataLoaderFactory {
+    constructor(connection) {
         this.db = connection;
     }
 
-    getLoaders() {        
+    getLoaders() {
         return {
-            dlcLoader : new DataLoader((ids)=> {
+            dlcLoader: new DataLoader((ids) => {
                 return DLCLoader.batchDlcs(this.db, ids)
             })
         }
