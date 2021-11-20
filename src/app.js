6efdb8b5282b90118c89cf660b5158d0509b3e37
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-20 13:05:12 -0300

    refactoring

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import getMessage from './getMessage';
+import * as requests from './requests';
 import { query, execute, connection } from './queries';
 import cors from 'cors';
 import lodash from 'lodash';
@@ -28,385 +28,39 @@ app.use('/graphql',
     req["context"].requestedFields = requestedFields;
     next();
   }
- ,graphqlHTTP((req)=> ({  
-    schema,  
+  , graphqlHTTP((req) => ({
+    schema,
     graphiql: true,
-    context: req['context']    
- })));
+    context: req['context']
+  })));
 
 
 //Here we are configuring express to use body-parser as middle-ware.
 
 const port = 4000;
 
-app.get('/', async (req, res) => {
-  res.send({ message: getMessage()})
-});
+app.get('/', requests.showWelcome);
+app.get('/test', requests.showTest);
+app.get('/statistics', requests.showStatistics);
+app.get('/origin', requests.showOriginGames);
+app.get('/ubisoft', requests.showUbisoftGames);
+app.get('/steam', requests.showSteamGames);
+app.get('/all', requests.showAllGames);
+app.get('/wii', requests.showWiiGames);
+app.get('/wiiu', requests.showWiiUGames);
+app.get('/pc', requests.showPCGames);
+app.get('/console', requests.showConsoleGames);
+app.get('/dlcs', requests.showDLCs)
 
-app.get('/test', async (req, res)=> {  
-  try {
-    let result =await query('SELECT * FROM [origins_games]')
-    res.status(200).send({result})
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  } 
-});
+app.post('/create', requests.createGames);
+app.post('/dlc_finished', requests.finishDLC);
+app.post('/finished', requests.finishGame);
+app.post('/search', requests.searchGame)
 
-app.get('/statistics', async (req,res) => {
-  console.log("from", req.query.from);
-  let q = ""
+app.put('/update', requests.updateGame);
 
-  switch (req.query.from) {
-    case 'finished':
-      q = 'SELECT * FROM [total_finished_games_for_dashboard];'  
-      break;
-    case 'totals':
-      q = `SELECT * FROM [total_games_for_dashboard];`
-      break;  
-    default:
-      q = 'SELECT 1'
-      break;
-  }  
-  
-  try {
-    const result = await query(q);    
-    res.status(200).send({result})
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }  
-});
+app.delete('/remove', requests.deleteGame);
 
-app.get('/origin', async (req, res) => {
-  try {
-    const games = await query('SELECT * FROM [origin_games]');
-    res.send({ games })
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }   
-});
-
-
-app.get('/ubisoft', async (req, res) => {
-  try {
-    const games = await query('SELECT * FROM [ubisoft_games]');
-    res.send({ games })
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }   
-});
-
-app.get('/steam', async (req, res) => {
-  try {
-    const games = await query('SELECT * FROM [all_steam_games]');
-
-    const mapped = lodash.map(games, (item) => {
-      return {finished: item.finished == 0 ? false : true, title: item.title, appid: item.appid, idx: item.idx }  
-    })
-  
-    res.send({ games: mapped })
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }   
-});
-
-
-app.get('/all', async (req, res) => {
-  try {
-    const games = await query('SELECT * FROM [all_games_list]');
-
-    const mapped = lodash.map(games, (item) => {
-      return {finished: item.finished == 0 ? false : true, title: item.title, system: item.system }  
-    })
-  
-    res.send({ games: mapped })
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }   
-});
-
-app.get('/wii', async (req, res) => {
-  try {
-    const games = await query('SELECT * FROM [wii_gc_games]');
-
-    res.send({ games })
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }   
-});
-
-app.get('/wiiu', async (req, res) => {
-  try {
-    const games = await query('SELECT * FROM [wiiu_games]');
-
-    res.send({ games })
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }   
-});
-
-app.get('/pc', async (req, res) => {
-  try {
-    const games = await query('SELECT * FROM [all_pc_games]');
-  
-    const mapped = lodash.map(games, (item) => {
-      return {finished: item.finished == 0 ? false : true, title: item.title, platform: item.platform }  
-    })
-  
-    res.send({ games: mapped })
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }   
-});
-
-app.get('/console', async (req, res) => {
-  try {
-    const games = await query('SELECT * FROM [all_console_games]');
-
-    res.send({ games })
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }   
-});
-
-const selectTable = (tableName) =>{
-  switch (tableName) {
-    case "wiiu":
-        return "wiiu_games"      
-    case "wii":
-      return "wii_gc_games"      
-    case "origin":
-      return "origin_games"      
-    case "ubisoft":
-      return "ubisoft_games"      
-    default:
-      return null;      
-  }
-}
-
-app.post('/create', async (req,res) => {  
-  
-  const tableName = req.body.table;
-  let table, title, finished,fisical_disc, id;
-
-  table = selectTable(tableName)
-
-    title = req.body.title;
-    finished = req.body.finished ? req.body.finished : false;
-    fisical_disc = req.body.fisical_disc ? req.body.fisical_disc : false;
-    id = req.body.id;
-
-    if (table == null){
-      const errorMessage ="Table does not match"; 
-      res.statusMessage = errorMessage;
-      res.status(400).send({msg: errorMessage}).end();
-    } else if (title == null || title == '' || title == undefined) {
-      const errorMessage ="Game Title is Empty"; 
-      res.statusMessage = errorMessage;
-      res.status(400).send({msg: errorMessage}).end();
-    } else if (finished == null) {
-      const errorMessage ="Finished is not Defined"; 
-      res.statusMessage = errorMessage;
-      res.status(400).send({msg: errorMessage}).end();
-    } else {
-      let q = "";
-
-      if(tableName === 'wii' || tableName === 'wiiu') {
-        q = `INSERT INTO [${table}] (id,title,finished,fisical_disc) VALUES ('${id}','${title}',${finished},${fisical_disc});`;
-      }else {
-        q = `INSERT INTO [${table}] (id,title,finished) VALUES ('${id}','${title}',${finished});`;
-      }
-
-      try {
-        const result = await execute(q);
-        
-        res.send({ ok: "ok" })
-      } catch (error) {
-        console.error(error)
-        res.status(400).send({msg: error.process.message}).end();
-      }      
-    }  
-});
-
-app.post('/dlc_finished', async (req, res) => {
-  const {idx, id, finished} = req.body
-  let q = `UPDATE [dlcs] SET [finished] = ${finished} WHERE [idx] = ${idx} AND [id] = '${id}';`;
-  try {
-    let result = await execute(q);      
-
-    q = `SELECT * FROM [dlcs] WHERE [id] = '${id}';`
-    
-    result = await query(q);
-
-    res.send({ dlcs: result });
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }       
-});
-
-app.post('/finished', async (req, res) => {  
-  
-  const tableName = req.body.table;
-  let table, title, finished, appid;
-
-  title = req.body.title;
-  appid = req.body.appid;
-  finished = req.body.finished;
-
-  table = selectTable(tableName)
-    
-  if (table == null){
-      const errorMessage ="Table does not match"; 
-      res.statusMessage = errorMessage;
-      res.status(400).send({msg: errorMessage}).end();
-    } else if (title == null || title == '' || title == undefined) {
-      const errorMessage ="Game Title is Empty"; 
-      res.statusMessage = errorMessage;
-      res.status(400).send({msg: errorMessage}).end();
-    } else if (finished == null) {
-      const errorMessage ="Finished is not Defined"; 
-      res.statusMessage = errorMessage;
-      res.status(400).send({msg: errorMessage}).end();
-    } else {
-
-      let q = "";
-
-      if(tableName === 'steam'){
-        q = `UPDATE [${table}] SET [finished] = ${finished} WHERE [appid] = ${appid};`;
-      } else {        
-        q = `UPDATE [${table}] SET [finished] = ${finished} WHERE [title] = '${title}';`;        
-      }      
-
-      try {
-        const result = await execute(q);      
-    
-        res.send({ result });
-      } catch (error) {
-        console.error(error)
-        res.status(400).send({msg: error.process.message}).end();
-      }       
-    }  
-});
-
-app.delete('/remove', async (req, res) => { 
-
-  const tableName = req.body.table;
-  const title = req.body.title;
-
-  let table;
-
-  table = selectTable(tableName)
-
-    if (table == null){
-      const errorMessage ="Table does not match"; 
-      res.statusMessage = errorMessage;
-      res.status(400).send({msg: errorMessage}).end();
-    } else if (title == null || title == '' || title == undefined) {
-      const errorMessage ="Game Title is Empty"; 
-      res.statusMessage = errorMessage;
-      res.status(400).send({msg: errorMessage}).end();
-    } else{
-      let q = "";
-      q = `DELETE FROM [${table}] WHERE [title] = '${title}';`;      
-    
-      try {
-        const result = await execute(q);
-  
-        res.send({ok:"ok"})
-      } catch (error) {
-        console.error(error)
-        res.status(400).send({msg: error.process.message}).end();
-      }       
-    }
-});
-
-app.put('/update', async (req, res) => {
-
-  const tableName = req.body.table;
-  let table, id, idx, title, finished, fisical_disc;
-
-  id = req.body.id;
-  idx = req.body.idx;
-  title = req.body.title;
-  finished = req.body.finished;
-  fisical_disc = req.body.fisical_disc;
-
-
-  table = selectTable(tableName)
-
-  if (table == null){
-    const errorMessage ="Table does not match"; 
-    res.statusMessage = errorMessage;
-    res.status(400).send({msg: errorMessage}).end();
-  } else if (title == null || title == '' || title == undefined) {
-    const errorMessage ="Game Title is Empty"; 
-    res.statusMessage = errorMessage;
-    res.status(400).send({msg: errorMessage}).end();
-  } else if (finished == null) {
-    const errorMessage ="Finished is not Defined"; 
-    res.statusMessage = errorMessage;
-    res.status(400).send({msg: errorMessage}).end();
-  } else if (idx == null) {
-    const errorMessage ="IDX is required"; 
-    res.statusMessage = errorMessage;
-    res.status(400).send({msg: errorMessage}).end();
-  } else {
-    let q = "";
-    
-    if(table === 'wiiu_games' || table === 'wii_gc_games') {
-      q = `UPDATE [${table}] SET [id] = '${id}',[title] = '${title}', [finished] = ${finished}, [fisical_disc] = ${fisical_disc} WHERE [idx] = ${idx};`;  
-    }else {
-      q = `UPDATE [${table}] SET [id] = '${id}',[title] = '${title}', [finished] = ${finished} WHERE [idx] = ${idx};`;  
-    }    
-
-    try {
-      const result = await execute(q);  
-  
-      res.send({ result }); 
-     
-    } catch (error) {
-      console.error(error)
-      res.status(400).send({msg: error.process.message}).end();
-    } 
-  
-    
-  }  
-});
-
-app.post('/search', async (req, res) => {
-  const q = req.body.query;     
-    try {
-       
-      const games = await query(`SELECT * FROM [all_games_list_api] WHERE title Like "*${q}*";`);
-
-      res.send({ games })    
-    } catch (error) {
-      console.error(error)
-      res.status(400).send({msg: error.process.message}).end();
-    }     
-})
-
-app.get('/dlcs', async (req, res) => {
-  const id = req.query.id;
-  console.log(id)
-  try {       
-    const dlcs = await query(`SELECT * FROM [dlcs] WHERE id = '${id}';`);
-    res.send({ dlcs })    
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({msg: error.process.message}).end();
-  }    
-})
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
---
+++ src/requests.js
@@ -0,0 +1,373 @@
+import getMessage from './getMessage';
+import { query, execute } from './queries';
+import lodash from 'lodash';
+
+const selectTable = (tableName) => {
+    switch (tableName) {
+        case "wiiu":
+            return "wiiu_games"
+        case "wii":
+            return "wii_gc_games"
+        case "origin":
+            return "origin_games"
+        case "ubisoft":
+            return "ubisoft_games"
+        default:
+            return null;
+    }
+}
+
+const showWelcome = (req, res) => {
+    res.send({ message: getMessage() })
+}
+
+const showTest = async (req, res) => {
+    try {
+        let result = await query('SELECT * FROM [origin_games]')
+        res.status(200).send({ result })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showStatistics = async (req, res) => {
+    console.log("from", req.query.from);
+    let q = ""
+
+    switch (req.query.from) {
+        case 'finished':
+            q = 'SELECT * FROM [total_finished_games_for_dashboard];'
+            break;
+        case 'totals':
+            q = `SELECT * FROM [total_games_for_dashboard];`
+            break;
+        default:
+            q = 'SELECT 1'
+            break;
+    }
+
+    try {
+        const result = await query(q);
+        res.status(200).send({ result })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showOriginGames = async (req, res) => {
+    try {
+        const games = await query('SELECT * FROM [origin_games]');
+        res.send({ games })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showUbisoftGames = async (req, res) => {
+    try {
+        const games = await query('SELECT * FROM [ubisoft_games]');
+        res.send({ games })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showSteamGames = async (req, res) => {
+    try {
+        const games = await query('SELECT * FROM [all_steam_games]');
+
+        const mapped = lodash.map(games, (item) => {
+            return { finished: item.finished == 0 ? false : true, title: item.title, appid: item.appid, idx: item.idx }
+        })
+
+        res.send({ games: mapped })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showAllGames = async (req, res) => {
+    try {
+        const games = await query('SELECT * FROM [all_games_list]');
+
+        const mapped = lodash.map(games, (item) => {
+            return { finished: item.finished == 0 ? false : true, title: item.title, system: item.system }
+        })
+
+        res.send({ games: mapped })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showWiiGames = async (req, res) => {
+    try {
+        const games = await query('SELECT * FROM [wii_gc_games]');
+
+        res.send({ games })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showWiiUGames = async (req, res) => {
+    try {
+        const games = await query('SELECT * FROM [wiiu_games]');
+
+        res.send({ games })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showPCGames = async (req, res) => {
+    try {
+        const games = await query('SELECT * FROM [all_pc_games]');
+
+        const mapped = lodash.map(games, (item) => {
+            return { finished: item.finished == 0 ? false : true, title: item.title, platform: item.platform }
+        })
+
+        res.send({ games: mapped })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showConsoleGames = async (req, res) => {
+    try {
+        const games = await query('SELECT * FROM [all_console_games]');
+
+        res.send({ games })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const showDLCs = async (req, res) => {
+    const id = req.query.id;
+    console.log(id)
+    try {
+        const dlcs = await query(`SELECT * FROM [dlcs] WHERE id = '${id}';`);
+        res.send({ dlcs })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const createGames = async (req, res) => {
+    const tableName = req.body.table;
+    let table, title, finished, fisical_disc, id;
+
+    table = selectTable(tableName)
+
+    title = req.body.title;
+    finished = req.body.finished ? req.body.finished : false;
+    fisical_disc = req.body.fisical_disc ? req.body.fisical_disc : false;
+    id = req.body.id;
+
+    if (table == null) {
+        const errorMessage = "Table does not match";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else if (title == null || title == '' || title == undefined) {
+        const errorMessage = "Game Title is Empty";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else if (finished == null) {
+        const errorMessage = "Finished is not Defined";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else {
+        let q = "";
+
+        if (tableName === 'wii' || tableName === 'wiiu') {
+            q = `INSERT INTO [${table}] (id,title,finished,fisical_disc) VALUES ('${id}','${title}',${finished},${fisical_disc});`;
+        } else {
+            q = `INSERT INTO [${table}] (id,title,finished) VALUES ('${id}','${title}',${finished});`;
+        }
+
+        try {
+            const result = await execute(q);
+
+            res.send({ ok: "ok" })
+        } catch (error) {
+            console.error(error)
+            res.status(400).send({ msg: error.process.message }).end();
+        }
+    }
+}
+const finishDLC = async (req, res) => {
+    const { idx, id, finished } = req.body
+    let q = `UPDATE [dlcs] SET [finished] = ${finished} WHERE [idx] = ${idx} AND [id] = '${id}';`;
+    try {
+        let result = await execute(q);
+
+        q = `SELECT * FROM [dlcs] WHERE [id] = '${id}';`
+
+        result = await query(q);
+
+        res.send({ dlcs: result });
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const finishGame = async (req, res) => {
+
+    const tableName = req.body.table;
+    let table, title, finished, appid;
+
+    title = req.body.title;
+    appid = req.body.appid;
+    finished = req.body.finished;
+
+    table = selectTable(tableName)
+
+    if (table == null) {
+        const errorMessage = "Table does not match";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else if (title == null || title == '' || title == undefined) {
+        const errorMessage = "Game Title is Empty";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else if (finished == null) {
+        const errorMessage = "Finished is not Defined";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else {
+
+        let q = "";
+
+        if (tableName === 'steam') {
+            q = `UPDATE [${table}] SET [finished] = ${finished} WHERE [appid] = ${appid};`;
+        } else {
+            q = `UPDATE [${table}] SET [finished] = ${finished} WHERE [title] = '${title}';`;
+        }
+
+        try {
+            const result = await execute(q);
+
+            res.send({ result });
+        } catch (error) {
+            console.error(error)
+            res.status(400).send({ msg: error.process.message }).end();
+        }
+    }
+}
+
+const searchGame = async (req, res) => {
+    const q = req.body.query;
+    try {
+
+        const games = await query(`SELECT * FROM [all_games_list_api] WHERE title Like "*${q}*";`);
+
+        res.send({ games })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({ msg: error.process.message }).end();
+    }
+}
+
+const updateGame = async (req, res) => {
+
+    const tableName = req.body.table;
+    let table, id, idx, title, finished, fisical_disc;
+
+    id = req.body.id;
+    idx = req.body.idx;
+    title = req.body.title;
+    finished = req.body.finished;
+    fisical_disc = req.body.fisical_disc;
+
+
+    table = selectTable(tableName)
+
+    if (table == null) {
+        const errorMessage = "Table does not match";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else if (title == null || title == '' || title == undefined) {
+        const errorMessage = "Game Title is Empty";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else if (finished == null) {
+        const errorMessage = "Finished is not Defined";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else if (idx == null) {
+        const errorMessage = "IDX is required";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else {
+        let q = "";
+
+        if (table === 'wiiu_games' || table === 'wii_gc_games') {
+            q = `UPDATE [${table}] SET [id] = '${id}',[title] = '${title}', [finished] = ${finished}, [fisical_disc] = ${fisical_disc} WHERE [idx] = ${idx};`;
+        } else {
+            q = `UPDATE [${table}] SET [id] = '${id}',[title] = '${title}', [finished] = ${finished} WHERE [idx] = ${idx};`;
+        }
+
+        try {
+            const result = await execute(q);
+
+            res.send({ result });
+
+        } catch (error) {
+            console.error(error)
+            res.status(400).send({ msg: error.process.message }).end();
+        }
+    }
+}
+
+const deleteGame = async (req, res) => {
+
+    const tableName = req.body.table;
+    const title = req.body.title;
+
+    let table;
+
+    table = selectTable(tableName)
+
+    if (table == null) {
+        const errorMessage = "Table does not match";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else if (title == null || title == '' || title == undefined) {
+        const errorMessage = "Game Title is Empty";
+        res.statusMessage = errorMessage;
+        res.status(400).send({ msg: errorMessage }).end();
+    } else {
+        let q = "";
+        q = `DELETE FROM [${table}] WHERE [title] = '${title}';`;
+
+        try {
+            const result = await execute(q);
+
+            res.send({ ok: "ok" })
+        } catch (error) {
+            console.error(error)
+            res.status(400).send({ msg: error.process.message }).end();
+        }
+    }
+}
+
+export {
+    showWelcome, showTest, showStatistics, showOriginGames, showUbisoftGames,
+    showSteamGames, showAllGames, showWiiGames, showWiiUGames, showPCGames,
+    showConsoleGames, showDLCs, createGames, finishDLC, finishGame, searchGame,
+    updateGame, deleteGame
+}
