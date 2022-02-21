9114b6f8c785fea5a3a512549923f0f22bdb9ef0
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-02-21 16:43:05 -0300

    created  model and migration for ToBuy Games

---
+++ src/migrations/20220221194201-create-to-buy.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ToBuy', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      finished: {
+        type: Sequelize.BOOLEAN
+      },
+      finished_at: {
+        type: Sequelize.DATE
+      },
+      genuine: {
+        type: Sequelize.BOOLEAN
+      },
+      system: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ToBuy');
+  }
+};
