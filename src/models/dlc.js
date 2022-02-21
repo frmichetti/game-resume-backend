6ee9fcee2edaf8498c4ab82ebca371ff56d7fdae
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-02-21 16:50:52 -0300

    created  model and migration for DLC Games

---
+++ src/models/dlc.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DLC extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  DLC.init({
+    app_id: DataTypes.STRING,
+    title: DataTypes.STRING,
+    finished: DataTypes.BOOLEAN,
+    finished_at: DataTypes.DATE,
+    collection: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'DLC',
+    tableName: 'DLC',
+  });
+  return DLC;
+};
