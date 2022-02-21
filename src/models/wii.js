bf7c2693416110d16d68da37fbfe7a3883e445eb
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-02-21 16:31:22 -0300

    created  model and migration for Wii Games

---
+++ src/models/wii.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Wii extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Wii.init({
+    app_id: DataTypes.STRING,
+    title: DataTypes.STRING,
+    finished: DataTypes.BOOLEAN,
+    finished_at: DataTypes.DATE,
+    collection: DataTypes.BOOLEAN,
+    genuine: DataTypes.BOOLEAN,
+    fisical_disc: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Wii',
+    tableName: 'Wii'
+  });
+  return Wii;
+};
