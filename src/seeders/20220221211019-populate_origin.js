62d25746db092ed56c6df90b834157a6dd716bab
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-02-21 18:30:16 -0300

    create seed for origin games

---
+++ src/seeders/20220221211019-populate_origin.js
@@ -0,0 +1,123 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Origin', [{
+      app_id: null,
+      title: 'Alice Madness Returns',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Burnout Paradise',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Crysis',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Crysis 2: Maximum Edition',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Crysis 3',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Crysis Warhead',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Dead Space 1',
+      finished: true,
+      finished_at: new Date('2021'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Dead Space 2',
+      finished: true,
+      finished_at: new Date('2021'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Dead Space 3',
+      finished: true,
+      finished_at: new Date('2021'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Fifa 18',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Kingdoms of Amalur Reckoning',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Need for Speed Hot Pursuit',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Need for Speed The Run',
+      finished: true,
+      finished_at: new Date('2021'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      app_id: null,
+      title: 'Star Wars Batlefront 2',
+      finished: false,
+      finished_at: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Origin', null, {});
+  }
+};
