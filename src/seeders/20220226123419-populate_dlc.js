c059556f6ba205a295a3d0ce783296955a1bc39a
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-02-26 09:40:56 -0300

    created seeds

---
+++ src/seeders/20220226123419-populate_dlc.js
@@ -0,0 +1,15 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const sql = fs.readFileSync(path.resolve(__dirname, 'DLC.sql'), 'utf8');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(sql);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('DLC', null, {});    
+  }
+};
