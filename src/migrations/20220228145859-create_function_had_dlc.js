5a1c2a1b243aaf22fa6633fb5675b9ff2acdb4fa
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-02-28 12:02:11 -0300

    create function has dlc

--- src/migrations/20220228145859-create_function_had_dlc.js
+++ src/migrations/20220228145859-create_function_had_dlc.js
@@ -1,21 +1,32 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    const ddl = `create function has_dlc(appid varchar)
+                    returns boolean
+                    language plpgsql
+                    as
+                    $$
+                    declare
+                      cnt integer;
+                      row_exists Boolean;
+                    begin
+                      row_exists := false;
+                    
+                      select count(*) INTO cnt from "DLC" where app_id=appid;
+                    
+                      IF cnt > 0 THEN
+                        row_exists := true;
+                      END IF;
+                    
+                      return row_exists;
+                    end;
+                    $$;`
+    await queryInterface.sequelize.query(ddl);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    const ddl = `drop function has_dlc(appid varchar);`
+    await queryInterface.sequelize.query(ddl);
   }
 };
