42338ca927a4936005cc0ab02634530a98011232
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-02-28 10:58:14 -0300

    fix null timestamps

--- src/seeders/20220228135254-populate_finished_at_in_null_values.js
+++ src/seeders/20220228135254-populate_finished_at_in_null_values.js
@@ -1,24 +1,19 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const sql = `UPDATE "Steam" set finished_at = '2021-06-01 00:00:00.0 +00:00' where finished = true AND finished_at is null;
+    UPDATE "Origin" set finished_at = '2021-06-01 00:00:00.0 +00:00' where finished = true AND finished_at is null;
+    UPDATE "Ubisoft" set finished_at = '2021-06-01 00:00:00.0 +00:00' where finished = true AND finished_at is null;
+    UPDATE "GameCube" set finished_at = '2021-06-01 00:00:00.0 +00:00' where finished = true AND finished_at is null;
+    UPDATE "Wii" set finished_at = '2021-06-01 00:00:00.0 +00:00' where finished = true AND finished_at is null;
+    UPDATE "WiiU" set finished_at = '2021-06-01 00:00:00.0 +00:00' where finished = true AND finished_at is null;
+    UPDATE "VirtualConsole" set finished_at = '2021-06-01 00:00:00.0 +00:00' where finished = true AND finished_at is null;
+    `
+    await queryInterface.sequelize.query(sql);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    
   }
 };
