4ddb3244cb720341f4329687af34d586c90141b5
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-01 15:49:35 -0300

    create views composed genres

--- src/migrations/20220301184427-create_views_composed_genres.js
+++ src/migrations/20220301184427-create_views_composed_genres.js
@@ -2,12 +2,24 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const ddl = ``
+    const ddl = `
+    create view total_of_console_genres_composed AS
+    SELECT gnr.genre, COUNT(gnr.app_id) AS TOTAL FROM
+          (SELECT string_agg(genre, ' - ') AS genre, app_id FROM console_games_genres group by app_id) gnr
+    GROUP BY gnr.genre;
+    
+    create view total_of_pc_genres_composed AS
+    SELECT gnr.genre, COUNT(gnr.app_id) AS TOTAL FROM
+          (SELECT string_agg(genre, ' - ') AS genre, app_id FROM pc_games_genres group by app_id) gnr
+    GROUP BY gnr.genre;
+    `
     await queryInterface.sequelize.query(ddl);
   },
 
   async down(queryInterface, Sequelize) {
-    const ddl = ``
+    const ddl = `
+    drop view total_of_pc_genres_composed;
+    drop view total_of_console_genres_composed;`
     await queryInterface.sequelize.query(ddl);
   }
 };
