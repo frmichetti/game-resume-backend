249358477b82fbe4ebd92477f55270420d4353fd
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-05 12:07:32 -0300

    fix

--- src/service.js
+++ src/service.js
@@ -4,7 +4,7 @@ var Service = require('node-windows').Service;
 var svc = new Service({
   name:'mgr_backend',
   description: 'Backend do My Games Resume',
-  script: 'D:\\DEV\\games-resume-backend\\build\\app.js'
+  script: 'C:\\DEV\\game-resume-backend\\build\\app.js'
 });
 
 // Listen for the "install" event, which indicates the
