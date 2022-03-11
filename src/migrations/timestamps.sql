1085bafe1b2a920f65c5873b8a6c212df8627c92
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-11 16:18:56 -0300

    fixed timestamps

--- src/migrations/timestamps.sql
+++ src/migrations/timestamps.sql
@@ -12,35 +12,22 @@ alter table "Category" alter column updated_at set default now();
 alter table "DLC" alter column created_at set default now();
 alter table "DLC" alter column updated_at set default now();
 
-alter table "GameCube" alter column created_at set default now();
-alter table "GameCube" alter column updated_at set default now();
+alter table "Games" alter column created_at set default now();
+alter table "Games" alter column updated_at set default now();
 
 alter table "GamesCategories" alter column created_at set default now();
 alter table "GamesCategories" alter column updated_at set default now();
 
-alter table "Origin" alter column created_at set default now();
-alter table "Origin" alter column updated_at set default now();
-
 alter table "Playing" alter column created_at set default now();
 alter table "Playing" alter column updated_at set default now();
 
-alter table "Steam" alter column created_at set default now();
-alter table "Steam" alter column updated_at set default now();
 
 alter table "ToBuy" alter column created_at set default now();
 alter table "ToBuy" alter column updated_at set default now();
 
-alter table "Ubisoft" alter column created_at set default now();
-alter table "Ubisoft" alter column updated_at set default now();
 
 alter table "VirtualConsole" alter column created_at set default now();
 alter table "VirtualConsole" alter column updated_at set default now();
-
-alter table "Wii" alter column created_at set default now();
-alter table "Wii" alter column updated_at set default now();
-
-alter table "WiiU" alter column created_at set default now();
-alter table "WiiU" alter column updated_at set default now();
 
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON "Category"
@@ -53,7 +40,7 @@ FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
 CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON "GameCube"
+BEFORE UPDATE ON "Games"
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
@@ -63,20 +50,9 @@ FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
 CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON "Origin"
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON "Playing"
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON "Steam"
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
 
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON "ToBuy"
@@ -84,21 +60,6 @@ FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
 CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON "Ubisoft"
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON "VirtualConsole"
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON "Wii"
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON "WiiU"
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
