3b8726c6ca31c6af72f45c2bd177992979d35bfe
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-12 19:10:58 -0300

    fixed tests

---
+++ src/tests/models/game.spec.js
@@ -0,0 +1,40 @@
+
+import * as db from '../../models/index';
+import { v4 as uuidv4 } from 'uuid';
+
+
+describe('tests with Game model', () => {
+    test('should to be reject to create a Game Model', ()  => {
+        
+        const payload = {            
+            app_id: "T3",
+            title: "Teste 3",
+            finished: true,
+            finished_at: new Date(),
+            system_id: 2,
+            collection: false,
+            genuine: true,
+            fisical_disc: false
+        }
+
+
+        expect(db.Game.create(payload)).rejects
+    });
+    
+    test('should to be resolves to create a Game Model', ()  => {
+        
+        const payload = {            
+            app_id: uuidv4(),
+            title: "Teste 3",
+            finished: false,
+            finished_at: null,
+            system_id: 2,
+            collection: false,
+            genuine: false,
+            fisical_disc: false
+        }
+
+
+        expect(db.Game.create(payload)).resolves
+    })
+})
