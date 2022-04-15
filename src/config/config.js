398876ceb814054a0bfb6c22e0bfd0946d20ed4e
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-04-15 15:01:29 -0300

    fix config

--- src/config/config.js
+++ src/config/config.js
@@ -26,21 +26,20 @@ module.exports = {
     }
   },
   "production": {
-    "username": "lioaqhdhoklmiz",
-    "password": "d0d9f4e9f13b302e2506988ee7ea7b4cfb684dbf9538bf3a77a8e9b95faa89da",
-    "database": "d6a9lt4gimo5ol",
-    "host": "ec2-3-224-125-117.compute-1.amazonaws.com",
-    "dialect": "postgres",
+    "use_env_variable": 'postgres://lioaqhdhoklmiz:d0d9f4e9f13b302e2506988ee7ea7b4cfb684dbf9538bf3a77a8e9b95faa89da@ec2-3-224-125-117.compute-1.amazonaws.com:5432/d6a9lt4gimo5ol',
+    "dialect": 'postgres',
+    "protocol": 'postgres',
     "dialectOptions": {
-      "ssl": { 
+      "ssl": {
         "require": true,
         "rejectUnauthorized": false,
       },
-    "migrationStorage": "sequelize",
-    "seederStorage": "sequelize",
-    "define": {
-      "timestamps": true,
-      "underscored": true
+      "migrationStorage": "sequelize",
+      "seederStorage": "sequelize",
+      "define": {
+        "timestamps": true,
+        "underscored": true
+      }
     }
   }
 }
